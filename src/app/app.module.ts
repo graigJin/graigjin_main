297dfe7bb7c25a9c75dbf55477900ec5f1214084
Sebastian Zimmermann <zimmermann.email@web.de>
2018-12-28 00:32:26 +0100

    Added bootstrap theme and split up into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { TopItemsComponent } from './top-items/top-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    CategoriesComponent,
+    CarouselComponent,
+    TopItemsComponent
   ],
   imports: [
     BrowserModule
